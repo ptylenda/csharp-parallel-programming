0e55d9278ea986e4f06eb8e1850f16b805a6b626
Piotr Tylenda <tylenda.piotr@gmail.com>
2018-05-14 10:57:52 +0200

    Initial commit

---
+++ Altkom.Intel.ParallelProgramming.Client/HelloWorldTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Altkom.Intel.ParallelProgramming.Client
+{
+    class HelloWorldTest
+    {
+        public static void Hello(string message)
+        {
+            Console.WriteLine("Sending...");
+            Thread.Sleep(TimeSpan.FromSeconds(3));
+            Console.WriteLine($"Sent: {message}");
+        }
+    }
+}
